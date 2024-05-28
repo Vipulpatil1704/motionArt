1e2ae3f9cd143b74f1cb4c770a9b315539ba192b
Vipulpatil1704 <vipul.d.patil1704@gmail.com>
2024-05-28 16:54:24 +0530

    third commit

--- public/script.js
+++ public/script.js
@@ -24,6 +24,7 @@ SOFTWARE.
 
 'use strict';
 
+// Mobile promo section
 
 
 // Simulation section
@@ -1446,18 +1447,57 @@ function correctRadius (radius) {
 // });
 
 // canvas.addEventListener('mousemove', e => {
-//     let pointer = pointers.find(p => p.id == -1);
-//     if (!pointer || !pointer.down) return;
+//     let pointer = pointers[0];
+//     if (!pointer.down) return;
 //     let posX = scaleByPixelRatio(e.offsetX);
 //     let posY = scaleByPixelRatio(e.offsetY);
 //     updatePointerMoveData(pointer, posX, posY);
 // });
 
 // window.addEventListener('mouseup', () => {
-//     let pointer = pointers.find(p => p.id == -1);
-//     if (pointer) updatePointerUpData(pointer);
+//     updatePointerUpData(pointers[0]);
 // });
 
+canvas.addEventListener('touchstart', e => {
+    e.preventDefault();
+    const touches = e.targetTouches;
+    while (touches.length >= pointers.length)
+        pointers.push(new pointerPrototype());
+    for (let i = 0; i < touches.length; i++) {
+        let posX = scaleByPixelRatio(touches[i].pageX);
+        let posY = scaleByPixelRatio(touches[i].pageY);
+        updatePointerDownData(pointers[i + 1], touches[i].identifier, posX, posY);
+    }
+});
+
+canvas.addEventListener('touchmove', e => {
+    e.preventDefault();
+    const touches = e.targetTouches;
+    for (let i = 0; i < touches.length; i++) {
+        let pointer = pointers[i + 1];
+        if (!pointer.down) continue;
+        let posX = scaleByPixelRatio(touches[i].pageX);
+        let posY = scaleByPixelRatio(touches[i].pageY);
+        updatePointerMoveData(pointer, posX, posY);
+    }
+}, false);
+
+window.addEventListener('touchend', e => {
+    const touches = e.changedTouches;
+    for (let i = 0; i < touches.length; i++)
+    {
+        let pointer = pointers.find(p => p.id == touches[i].identifier);
+        if (pointer == null) continue;
+        updatePointerUpData(pointer);
+    }
+});
+
+window.addEventListener('keydown', e => {
+    if (e.code === 'KeyP')
+        config.PAUSED = !config.PAUSED;
+    if (e.key === ' ')
+        splatStack.push(parseInt(Math.random() * 20) + 5);
+});
 canvas.addEventListener('pointermove', e => {
     if (e.pointerType !== 'mouse') return; // Only handle mouse events
     let pointer = pointers.find(p => p.id === -1);
@@ -1472,14 +1512,6 @@ canvas.addEventListener('pointermove', e => {
         updatePointerMoveData(pointer, posX, posY);
     }
 });
-
-window.addEventListener('keydown', e => {
-    if (e.code === 'KeyP')
-        config.PAUSED = !config.PAUSED;
-    if (e.key === ' ')
-        splatStack.push(parseInt(Math.random() * 20) + 5);
-});
-
 function updatePointerDownData (pointer, id, posX, posY) {
     pointer.id = id;
     pointer.down = true;
